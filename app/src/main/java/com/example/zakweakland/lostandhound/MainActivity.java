6b1109ff60b13fc1cd7f142a4007650696d92f9d
Zak weakland <zakweakland@Zaks-MacBook-Air.local>
2019-02-27 16:10:47 -0500

    First commit

---
+++ app/src/main/java/com/example/zakweakland/lostandhound/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zakweakland.lostandhound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+}
+
