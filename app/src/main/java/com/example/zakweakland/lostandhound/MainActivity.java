805ef1a870719b442e46ca4395a389ac6572e894
Zak weakland <zakweakland@Zaks-MacBook-Air.local>
2019-03-12 22:09:04 -0400

    edit main activity and layout

--- app/src/main/java/com/example/zakweakland/lostandhound/MainActivity.java
+++ app/src/main/java/com/example/zakweakland/lostandhound/MainActivity.java
@@ -1,24 +1,113 @@
 package com.example.zakweakland.lostandhound;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
+    private Button buttonRegister;
+    private Button alreadyRegisterLogin;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        buttonRegister = findViewById(R.id.registerButton);
+        alreadyRegisterLogin = findViewById(R.id.login);
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+
+        buttonRegister.setOnClickListener(this);
+        alreadyRegisterLogin.setOnClickListener(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
 
 
     }
 
+
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+            // email is empty
+            Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            // email is empty
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering... Please wait...");
+        progressDialog.show();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressDialog.dismiss();
+            }
+        }, 1500);
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                            Toast.makeText(MainActivity.this,"Registered Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(loginIntent);
+                        }
+                        if(!task.isSuccessful()){
+                            FirebaseAuthException e = (FirebaseAuthException )task.getException();
+                            Toast.makeText(MainActivity.this, "Failed Registration: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttonRegister){
+            registerUser();
+        }
+        if (v == alreadyRegisterLogin){
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+        }
+
+    }
 }
 
